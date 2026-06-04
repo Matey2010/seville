468cb57b47096688ce3db5fcd5f34ca61bd66bcf
Maksym Teplyy <mxtey@proton.me>
2026-06-04 19:16:54 +0200

    Add proto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:math';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,116 +12,141 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Seville',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameScreen> createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _GameScreenState extends State<GameScreen> {
+  late final TapGame _game = TapGame();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          GameWidget(game: _game),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: SizedBox(
+                height: 36,
+                child: ElevatedButton(
+                  onPressed: _game.reset,
+                  child: const Text('Reset'),
+                ),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
+  }
+}
+
+/// A tiny game: tap the moving ball to score. It speeds up each hit.
+class TapGame extends FlameGame {
+  late final Ball _ball;
+  late final TextComponent _scoreText;
+  int _score = 0;
+
+  @override
+  Color backgroundColor() => const Color(0xFF1A1228);
+
+  @override
+  Future<void> onLoad() async {
+    _scoreText = TextComponent(
+      text: 'Score: 0',
+      position: Vector2(16, 48),
+      textRenderer: TextPaint(
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+    add(_scoreText);
+
+    _ball = Ball(onHit: _onHit)..position = size / 2;
+    add(_ball);
+  }
+
+  void _onHit() {
+    _score++;
+    _scoreText.text = 'Score: $_score';
+    _ball.speedUp();
+  }
+
+  void reset() {
+    _score = 0;
+    _scoreText.text = 'Score: 0';
+    _ball.resetState(size / 2);
+  }
+}
+
+class Ball extends CircleComponent with TapCallbacks {
+  Ball({required this.onHit})
+      : super(
+          radius: 32,
+          anchor: Anchor.center,
+          paint: Paint()..color = Colors.amber,
+        );
+
+  final VoidCallback onHit;
+  final Random _rng = Random();
+  late Vector2 _velocity = _randomVelocity(180);
+
+  Vector2 _randomVelocity(double speed) {
+    final angle = _rng.nextDouble() * 2 * pi;
+    return Vector2(cos(angle), sin(angle))..scale(speed);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += _velocity * dt;
+
+    // Bounce off the walls.
+    final game = findGame()!;
+    if (position.x - radius < 0 || position.x + radius > game.size.x) {
+      _velocity.x = -_velocity.x;
+      position.x = position.x.clamp(radius, game.size.x - radius);
+    }
+    if (position.y - radius < 0 || position.y + radius > game.size.y) {
+      _velocity.y = -_velocity.y;
+      position.y = position.y.clamp(radius, game.size.y - radius);
+    }
+  }
+
+  void speedUp() {
+    _velocity.scale(1.12);
+  }
+
+  void resetState(Vector2 center) {
+    position = center;
+    _velocity = _randomVelocity(180);
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    onHit();
   }
 }
